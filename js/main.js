33aed097d0ce75ee8f1ef61e34d4be75d3ffbcb9
Alejandra Saldana <xochipillitx@gmail.com>
2018-01-13 10:52:16 -0600

    Added parallax effect to intro, Project cards and Project title card.

--- js/main.js
+++ js/main.js
@@ -31,4 +31,55 @@ $(document).ready(function () {
     });
 
   // ScrollMagic //
+
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+  // Pin Intro
+  var pinIntroScene = new ScrollMagic.Scene({
+      triggerElement: '#intro',
+      triggerHook: 0,
+    })
+    .setPin('#intro', {
+      pushFollowers: false,
+    })
+    .addTo(controller);
+
+  // parallax scene
+
+  var parallaxTL = new TimelineMax();
+  parallaxTL
+    .from('.content-wrapper', 0.4, {
+      autoAlpha: 0,
+      ease: Power0.easeNone,
+    }, 0.4)
+    .from('.bcg', 2, {
+      y: '-30%',
+      ease: Power0.easeNone,
+    }, 0);
+
+  var slideParallaxScene = new ScrollMagic.Scene({
+      triggerElement: '.bcg-parallax',
+      triggerHook: 1,
+      duration: '200%',
+    })
+    .setTween(parallaxTL)
+    .addTo(controller);
+
+  //loop through each project element
+  $('.project').each(function () {
+    // build a scene
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement: this.children[0],
+        triggerHook: 0.9,
+      })
+      .setClassToggle(this, 'fade-in') // add class to project 01
+      .addIndicators({
+        name: 'fade scene',
+        colorTrigger: 'black',
+        colorStart: '#75c695',
+        colorEnd: 'pink',
+      })
+      .addTo(controller);
+  });
 });
